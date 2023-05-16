593783ab6eec4f30f604663f0736f093ff71fb1e
im linux <imlinux68@gmail.com>
2023-05-16 15:19:50 +0300

    added route

--- main.tf
+++ main.tf
@@ -34,3 +34,9 @@ resource "aws_route_table" "MyMainRT" {
     "Name" = "MyDefault gateway"
   }
 }
+
+resource "aws_route" "def_route" {
+    route_table_id = aws_route_table.MyMainRT.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+}
